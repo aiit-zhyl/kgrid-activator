be0f3dd26de67c6a6dddaa066f6ac2d960519e13
Greg Farris <greg.t.farris@gmail.com>
2018-08-24 15:39:09 -0400

    https://www.pivotaltracker.com/story/show/160029026
    Changed the Endpoint decorator to use HttpServletRequest

--- src/main/java/org/kgrid/activator/controller/EndpointKnowledgeObjectDecorator.java
+++ src/main/java/org/kgrid/activator/controller/EndpointKnowledgeObjectDecorator.java
@@ -1,14 +1,12 @@
 package org.kgrid.activator.controller;
 
-import org.kgrid.activator.EndPoint;
+import javax.servlet.http.HttpServletRequest;
 import org.kgrid.activator.services.ActivationService;
 import org.kgrid.activator.services.ServiceDescriptionService;
 import org.kgrid.shelf.controller.KnowledgeObjectDecorator;
-import org.kgrid.shelf.domain.ArkId;
 import org.kgrid.shelf.domain.KnowledgeObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
-import org.springframework.http.RequestEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -22,13 +20,13 @@ class EndpointKnowledgeObjectDecorator implements KnowledgeObjectDecorator {
   private ServiceDescriptionService serviceDescriptionService;
 
   @Override
-  public void decorate(KnowledgeObject knowledgeObject, RequestEntity requestEntity) {
+  public void decorate(KnowledgeObject knowledgeObject, HttpServletRequest httpServletRequest) {
 
     if (activationService.getEndpoints().containsKey(activationService.getKnowleledgeObjectPath(
         knowledgeObject)+ activationService.getEndPointPath(knowledgeObject))) {
 
       knowledgeObject.getMetadata().put("endpoint",
-          requestEntity.getUrl() + activationService.getEndPointPath(
+          httpServletRequest.getRequestURL() + activationService.getEndPointPath(
               knowledgeObject));
     }
   }
